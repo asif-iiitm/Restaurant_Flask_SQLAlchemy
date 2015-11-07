7b2a0ebe68b3cea854a8035bf9a2d6f8fea6e7dd
uchiha <asif.iiitm@gmail.com>
2015-11-07 21:44:04 +0530

    Add Database_setup.py

---
+++ database_setup.py
@@ -0,0 +1,24 @@
+import sys
+form sqlalchemy import Column , ForeignKey , Integer , String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base=declarative_base()
+
+class Reataurant(Base):
+	__tablename__ = 'restaurant'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), null=False)
+
+class MenuItem(Base):
+	__tablename__= 'menu_item'
+
+	name = Column(String(80), nullable=False)
+	id = Column(Integer , primary_key = True)
+	description = Column(String(250))
+	price = Column(String(8))
+
+
+engine =create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
